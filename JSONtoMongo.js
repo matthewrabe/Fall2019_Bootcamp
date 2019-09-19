8465c5757e25e76a2248d55b116485b8da0faabf
Matt <matthewrabe@ufl.edu>
2019-09-18 20:23:10 -0400

    Final upload of bootcamp2 files

--- JSONtoMongo.js
+++ JSONtoMongo.js
@@ -12,6 +12,7 @@ var fs = require('fs'),
 /* Connect to your database using mongoose - remember to keep your key secret*/
 //see https://mongoosejs.com/docs/connections.html
 //See https://docs.atlas.mongodb.com/driver-connection/
+mongoose.connect(config.db.uri, {useNewUrlParser: true});
 
 /* 
   Instantiate a mongoose model for each listing object in the JSON file, 
@@ -20,7 +21,30 @@ var fs = require('fs'),
 
   Remember that we needed to read in a file like we did in Bootcamp Assignment #1.
  */
+fs.readFile('listings.json', 'utf8', function(err, data) {
 
+    //Check for errors
+  if(err) throw err;
+
+  var listingArray = JSON.parse(data);
+
+   //Save the state
+  listingArray["entries"].forEach(function(element) {
+    var currentListing = new Listing(element);
+    currentListing.save(function (err) {
+      if (err) throw err;
+    })
+  });
+
+  /*//Creates the server
+  server = http.createServer(requestHandler);
+
+  //Start the server
+  server.listen(port, function() {
+    console.log('Server listening on: http://localhost:' + port);
+  })*/
+
+});
 
 /*  
   Check to see if it works: Once you've written + run the script, check out your MongoLab database to ensure that 
